ff9447805c793b18a35fa596967be1d4a758eb9b
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-07 21:52:07 +0100

    multiply

---
+++ src/multiply/index.ts
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/50654ddff44f800200000004
+
+This code does not execute properly. Try to figure out why.
+ */
+
+export function multiply(a: number, b: number): number {
+    // return a + b;
+    return a * b;
+}
