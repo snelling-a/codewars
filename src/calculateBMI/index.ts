ea7ad4c0a062d3e6782b15d2896bce7c607cc493
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-08 07:26:56 +0100

    calculate bmi

---
+++ src/calculateBMI/index.ts
@@ -0,0 +1,32 @@
+/*
+https://www.codewars.com/kata/57a429e253ba3381850000fb/train/typescript
+
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+ */
+
+export function bodyMassIndex(weight: number, height: number): string {
+    const underweight = 18.5;
+    const normal = 25;
+    const overweight = 30;
+
+    const bmi = weight / (height * height);
+
+    switch (true) {
+        case bmi <= underweight:
+            return 'Underweight';
+        case bmi <= normal:
+            return 'Normal';
+        case bmi <= overweight:
+            return 'Overweight';
+        default:
+            return 'Obese';
+    }
+}
