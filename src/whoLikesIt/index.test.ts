bde5d1e7339084db64980119dc7c484f2684be24
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-08 16:03:44 +0100

    who likes it (#6)

---
+++ src/whoLikesIt/index.test.ts
@@ -0,0 +1,19 @@
+import { likes } from '.';
+
+describe(likes.name, () => {
+    it.each([
+        [[], 'no one likes this'],
+        [['Peter'], 'Peter likes this'],
+        [['Jacob', 'Alex'], 'Jacob and Alex like this'],
+        [['Max', 'John', 'Mark'], 'Max, John and Mark like this'],
+        [
+            ['Alex', 'Jacob', 'Mark', 'Max'],
+            'Alex, Jacob and 2 others like this',
+        ],
+    ])(
+        'given %s people who like a post, should return "%s"',
+        (people, output) => {
+            expect(likes(people)).toEqual(output);
+        },
+    );
+});
