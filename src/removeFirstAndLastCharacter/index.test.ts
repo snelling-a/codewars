2dcddc8b9cd9d74f6a536defc23d6ebcb00baf04
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-08 16:18:13 +0100

    random tests passed

--- src/removeFirstAndLastCharacter/index.test.ts
+++ src/removeFirstAndLastCharacter/index.test.ts
@@ -8,7 +8,16 @@ describe(removeChar.name, () => {
         ['place', 'lac'],
     ];
 
-    it.each(basicTests)(
+    const randomTests = [
+        ['211c212', '11c21'],
+        ['c2bac11', '2bac1'],
+        ['1b31ab3', 'b31ab'],
+        ['a22222c', '22222'],
+        ['13bb131', '3bb13'],
+        ['2c321c1', 'c321c'],
+    ];
+
+    it.each([...basicTests, ...randomTests])(
         'should take in "%s" and output "%s"',
         (original, removed) => {
             expect(removeChar(original)).toEqual(removed);
