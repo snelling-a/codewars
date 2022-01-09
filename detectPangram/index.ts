fea29574615ac47b6f5b08d41599a08853adfcf1
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-09 09:01:41 +0100

    author's solution in comment

--- detectPangram/index.ts
+++ detectPangram/index.ts
@@ -14,3 +14,8 @@ export const isPangram = (phrase: string): boolean => {
 
     return regex.test(phrase);
 };
+
+/*
+const isPangramAuthor = (phrase: string): boolean => ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  .every(l => phrase.toLowerCase().includes(l))
+*/
