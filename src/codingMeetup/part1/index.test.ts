91f58f4acb755f346ee017ddd49a2b5a68236ac8
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-09 21:14:01 +0100

    Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe (#18)
    * Count the number of JavaScript developers coming from Europe init
    
    * tests passing
    
    * additional tests

---
+++ src/codingMeetup/part1/index.test.ts
@@ -0,0 +1,72 @@
+import { countDevelopers, Developer } from '.';
+import { fullList } from './fullList';
+
+type TestCase = [Developer[], number];
+
+describe(countDevelopers.name, () => {
+    const testCase1: TestCase = [
+        [
+            {
+                firstName: 'Noah',
+                lastName: 'M.',
+                country: 'Switzerland',
+                continent: 'Europe',
+                age: 19,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Maia',
+                lastName: 'S.',
+                country: 'Tahiti',
+                continent: 'Oceania',
+                age: 28,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Shufen',
+                lastName: 'L.',
+                country: 'Taiwan',
+                continent: 'Asia',
+                age: 35,
+                language: 'HTML',
+            },
+            {
+                firstName: 'Sumayah',
+                lastName: 'M.',
+                country: 'Tajikistan',
+                continent: 'Asia',
+                age: 30,
+                language: 'CSS',
+            },
+        ],
+        1,
+    ];
+
+    const testCase2: TestCase = [
+        [
+            {
+                firstName: 'Oliver',
+                lastName: 'Q.',
+                country: 'Australia',
+                continent: 'Oceania',
+                age: 19,
+                language: 'HTML',
+            },
+            {
+                firstName: 'Lukas',
+                lastName: 'R.',
+                country: 'Austria',
+                continent: 'Europe',
+                age: 89,
+                language: 'HTML',
+            },
+        ],
+        0,
+    ];
+
+    it.each<TestCase>([testCase1, testCase2, [fullList, 8]])(
+        'should count the number of JS developers form Europe',
+        (test, jsDevsFromEurope) =>
+            expect(countDevelopers(test)).toEqual(jsDevsFromEurope),
+    );
+});
