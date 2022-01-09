5273041421577099aa587f27f2929149f89223b5
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-09 10:16:51 +0100

    additional tests

--- src/decodeTheMorseCode/part1/index.test.ts
+++ src/decodeTheMorseCode/part1/index.test.ts
@@ -3,7 +3,28 @@ import { decodeMorse } from '.';
 describe(decodeMorse.name, () => {
     const testCase = ['.... . -.--   .--- ..- -.. .', 'HEY JUDE'];
 
-    it.each([testCase])('should decode "%s" into "%s"', (morse, english) =>
-        expect(decodeMorse(morse)).toEqual(english),
+    const basicMorseDecoding = [
+        ['.-', 'A'],
+        ['.', 'E'],
+        ['..', 'I'],
+        ['. .', 'EE'],
+        ['.   .', 'E E'],
+        ['...---...', 'SOS'],
+        ['... --- ...', 'SOS'],
+        ['...   ---   ...', 'S O S'],
+    ];
+
+    const moreComplexTests = [
+        [' . ', 'E'],
+        ['   .   . ', 'E E'],
+        [
+            '      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-     ',
+            'SOS! THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.',
+        ],
+    ];
+
+    it.each([testCase, ...basicMorseDecoding, ...moreComplexTests])(
+        'should decode "%s" into "%s"',
+        (morse, english) => expect(decodeMorse(morse)).toEqual(english),
     );
 });
