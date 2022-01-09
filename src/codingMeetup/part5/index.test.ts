f7c0035172cdb51be6495a4a7b29125fb87c200c
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-09 23:07:40 +0100

    Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages (#22)
    * Coding Meetup #5 - Higher-Order Functions Series - Prepare the count of languages init
    
    * test passing
    
    * additional test

---
+++ src/codingMeetup/part5/index.test.ts
@@ -0,0 +1,64 @@
+import { countLanguages } from '.';
+import { fullList } from '../fullList';
+import { Developer, LanguageCounts } from '../types';
+
+type TestCase = [Developer[], LanguageCounts];
+
+describe(countLanguages.name, () => {
+    const testCase1: TestCase = [
+        [
+            {
+                firstName: 'Noah',
+                lastName: 'M.',
+                country: 'Switzerland',
+                continent: 'Europe',
+                age: 19,
+                language: 'C',
+            },
+            {
+                firstName: 'Anna',
+                lastName: 'R.',
+                country: 'Liechtenstein',
+                continent: 'Europe',
+                age: 52,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Ramon',
+                lastName: 'R.',
+                country: 'Paraguay',
+                continent: 'Americas',
+                age: 29,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'George',
+                lastName: 'B.',
+                country: 'England',
+                continent: 'Europe',
+                age: 81,
+                language: 'C',
+            },
+        ],
+        { C: 2, JavaScript: 1, Ruby: 1 },
+    ];
+
+    const testCase2: TestCase = [
+        fullList,
+        {
+            C: 10,
+            Clojure: 8,
+            Java: 16,
+            JavaScript: 14,
+            PHP: 9,
+            Python: 20,
+            Ruby: 20,
+        },
+    ];
+
+    it.each([testCase1, testCase2])(
+        'should count the languages',
+        (devList, langCount) =>
+            expect(countLanguages(devList)).toEqual(langCount),
+    );
+});
---
+++ src/codingMeetup/part5/index.ts
@@ -0,0 +1,38 @@
+/*
+https://www.codewars.com/kata/coding-meetup-number-5-higher-order-functions-series-prepare-the-count-of-languages
+
+You will be given an array of objects representing data about developers who have signed up to attend the next
+coding meetup that you are organising.
+
+Your task is to return an object which includes the count of each coding language represented at the meetup.
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+];
+your function should return the following object:
+
+{ C: 2, JavaScript: 1, Ruby: 1 }
+Notes:
+    The order of the languages in the object does not matter.
+    The count value should be a valid number.
+    The input array will always be valid and formatted as in the example above.
+*/
+
+import { Developer, LanguageCounts } from '../types';
+
+export function countLanguages(list: Developer[]) {
+    return list.reduce<LanguageCounts>((languages, developer) => {
+        const devLang = languages[developer.language];
+
+        if (devLang) {
+            return { ...languages, [developer.language]: devLang + 1 };
+        }
+
+        return { ...languages, [developer.language]: 1 };
+    }, {});
+}
