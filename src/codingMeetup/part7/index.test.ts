06649874e9ac4258010b8a4ad15302cbec020f3e
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-10 14:45:20 +0100

    additional tests

--- src/codingMeetup/part7/index.test.ts
+++ src/codingMeetup/part7/index.test.ts
@@ -1,4 +1,5 @@
 import { findSenior } from '.';
+import { fullList } from '../fullList';
 import { Developer } from '../types';
 
 type TestCase = [Developer[], Developer[]];
@@ -192,8 +193,290 @@ describe(findSenior.name, () => {
         ],
     ];
 
-    it.each([testCase1, testCase2, testCase3])(
-        'should find the oldest developer(s)',
-        (list, oldest) => expect(findSenior(list)).toEqual(oldest),
+    const testCase4: TestCase = [
+        [
+            {
+                firstName: 'Mark',
+                lastName: 'G.',
+                country: 'Scotland',
+                continent: 'Europe',
+                age: 22,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Victoria',
+                lastName: 'T.',
+                country: 'Puerto Rico',
+                continent: 'Americas',
+                age: 30,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Emma',
+                lastName: 'B.',
+                country: 'Norway',
+                continent: 'Europe',
+                age: 19,
+                language: 'Clojure',
+            },
+        ],
+        [
+            {
+                firstName: 'Victoria',
+                lastName: 'T.',
+                country: 'Puerto Rico',
+                continent: 'Americas',
+                age: 30,
+                language: 'Clojure',
+            },
+        ],
+    ];
+
+    const testCase5: TestCase = [
+        [
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Europe',
+                age: 29,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Amar',
+                lastName: 'V.',
+                country: 'Bosnia and Herzegovina',
+                continent: 'Europe',
+                age: 32,
+                language: 'Ruby',
+            },
+        ],
+        [
+            {
+                firstName: 'Amar',
+                lastName: 'V.',
+                country: 'Bosnia and Herzegovina',
+                continent: 'Europe',
+                age: 32,
+                language: 'Ruby',
+            },
+        ],
+    ];
+
+    const testCase6: TestCase = [
+        [
+            {
+                firstName: 'Sofia',
+                lastName: 'P.',
+                country: 'Italy',
+                continent: 'Europe',
+                age: 41,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Jayden',
+                lastName: 'P.',
+                country: 'Jamaica',
+                continent: 'Americas',
+                age: 42,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Sou',
+                lastName: 'B.',
+                country: 'Japan',
+                continent: 'Asia',
+                age: 43,
+                language: 'Python',
+            },
+            {
+                firstName: 'Rimas',
+                lastName: 'C.',
+                country: 'Jordan',
+                continent: 'Asia',
+                age: 44,
+                language: 'Java',
+            },
+        ],
+        [
+            {
+                firstName: 'Rimas',
+                lastName: 'C.',
+                country: 'Jordan',
+                continent: 'Asia',
+                age: 44,
+                language: 'Java',
+            },
+        ],
+    ];
+
+    const testCase7: TestCase = [
+        [
+            {
+                firstName: 'Alexander',
+                lastName: 'F.',
+                country: 'Russia',
+                continent: 'Europe',
+                age: 89,
+                language: 'Java',
+            },
+            {
+                firstName: 'Fatima',
+                lastName: 'K.',
+                country: 'Saudi Arabia',
+                continent: 'Asia',
+                age: 21,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Mark',
+                lastName: 'G.',
+                country: 'Scotland',
+                continent: 'Europe',
+                age: 22,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Nikola',
+                lastName: 'H.',
+                country: 'Serbia',
+                continent: 'Europe',
+                age: 29,
+                language: 'Python',
+            },
+            {
+                firstName: 'Jakub',
+                lastName: 'I.',
+                country: 'Slovakia',
+                continent: 'Europe',
+                age: 28,
+                language: 'Java',
+            },
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Europe',
+                age: 89,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Luka',
+                lastName: 'J.',
+                country: 'Slovenia',
+                continent: 'Europe',
+                age: 29,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Mariam',
+                lastName: 'B.',
+                country: 'Egypt',
+                continent: 'Africa',
+                age: 89,
+                language: 'Python',
+            },
+        ],
+        [
+            {
+                firstName: 'Alexander',
+                lastName: 'F.',
+                country: 'Russia',
+                continent: 'Europe',
+                age: 89,
+                language: 'Java',
+            },
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Europe',
+                age: 89,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Mariam',
+                lastName: 'B.',
+                country: 'Egypt',
+                continent: 'Africa',
+                age: 89,
+                language: 'Python',
+            },
+        ],
+    ];
+
+    const testCase8: TestCase = [
+        [
+            {
+                firstName: 'Mehdi',
+                lastName: 'H.',
+                country: 'Tunisia',
+                continent: 'Africa',
+                age: 42,
+                language: 'Python',
+            },
+            {
+                firstName: 'Yusuf',
+                lastName: 'N.',
+                country: 'Turkey',
+                continent: 'Europe',
+                age: 22,
+                language: 'Python',
+            },
+        ],
+        [
+            {
+                firstName: 'Mehdi',
+                lastName: 'H.',
+                country: 'Tunisia',
+                continent: 'Africa',
+                age: 42,
+                language: 'Python',
+            },
+        ],
+    ];
+
+    const fullListTest: TestCase = [
+        fullList,
+        [
+            {
+                age: 89,
+                continent: 'Europe',
+                country: 'Austria',
+                firstName: 'Lukas',
+                language: 'C',
+                lastName: 'R.',
+            },
+            {
+                age: 89,
+                continent: 'Africa',
+                country: 'Egypt',
+                firstName: 'Mariam',
+                language: 'Python',
+                lastName: 'B.',
+            },
+            {
+                age: 89,
+                continent: 'Europe',
+                country: 'Russia',
+                firstName: 'Alexander',
+                language: 'Java',
+                lastName: 'F.',
+            },
+        ],
+    ];
+
+    it.each([
+        testCase1,
+        testCase2,
+        testCase3,
+        testCase4,
+        testCase5,
+        testCase6,
+        testCase7,
+        testCase8,
+        fullListTest,
+    ])('should find the oldest developer(s)', (list, oldest) =>
+        expect(findSenior(list)).toEqual(oldest),
     );
 });
