935fb7746368a3c0bbf532a0e020c1019d4ec6ef
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-10 21:21:29 +0100

    Coding Meetup #9 - Higher-Order Functions Series - Is the meetup age-diverse? init

---
+++ src/codingMeetup/part9/index.test.ts
@@ -0,0 +1,156 @@
+import { isAgeDiverse } from '.';
+import { TestCaseBoolean } from '../types';
+
+describe(isAgeDiverse.name, () => {
+    const testCase1: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Harry',
+                lastName: 'K.',
+                country: 'Brazil',
+                continent: 'Americas',
+                age: 19,
+                language: 'Python',
+            },
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Europe',
+                age: 29,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Jing',
+                lastName: 'X.',
+                country: 'China',
+                continent: 'Asia',
+                age: 39,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Noa',
+                lastName: 'A.',
+                country: 'Israel',
+                continent: 'Asia',
+                age: 40,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Andrei',
+                lastName: 'E.',
+                country: 'Romania',
+                continent: 'Europe',
+                age: 59,
+                language: 'C',
+            },
+            {
+                firstName: 'Maria',
+                lastName: 'S.',
+                country: 'Peru',
+                continent: 'Americas',
+                age: 60,
+                language: 'C',
+            },
+            {
+                firstName: 'Lukas',
+                lastName: 'X.',
+                country: 'Croatia',
+                continent: 'Europe',
+                age: 75,
+                language: 'Python',
+            },
+            {
+                firstName: 'Chloe',
+                lastName: 'K.',
+                country: 'Guernsey',
+                continent: 'Europe',
+                age: 88,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Viktoria',
+                lastName: 'W.',
+                country: 'Bulgaria',
+                continent: 'Europe',
+                age: 98,
+                language: 'PHP',
+            },
+            {
+                firstName: 'Piotr',
+                lastName: 'B.',
+                country: 'Poland',
+                continent: 'Europe',
+                age: 128,
+                language: 'JavaScript',
+            },
+        ],
+        true,
+    ];
+
+    const testCase2: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Europe',
+                age: 29,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Amar',
+                lastName: 'V.',
+                country: 'Bosnia and Herzegovina',
+                continent: 'Europe',
+                age: 32,
+                language: 'Ruby',
+            },
+        ],
+        false,
+    ];
+
+    const testCase3: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Sofia',
+                lastName: 'P.',
+                country: 'Italy',
+                continent: 'Europe',
+                age: 41,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Jayden',
+                lastName: 'P.',
+                country: 'Jamaica',
+                continent: 'Americas',
+                age: 42,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Sou',
+                lastName: 'B.',
+                country: 'Japan',
+                continent: 'Asia',
+                age: 43,
+                language: 'Python',
+            },
+            {
+                firstName: 'Rimas',
+                lastName: 'C.',
+                country: 'Jordan',
+                continent: 'Asia',
+                age: 44,
+                language: 'Java',
+            },
+        ],
+        false,
+    ];
+
+    it.each([testCase1, testCase2, testCase3])(
+        'should return true if all age groups are represented',
+        (developers, allAgesRepresented) =>
+            expect(isAgeDiverse(developers)).toEqual(allAgesRepresented),
+    );
+});
