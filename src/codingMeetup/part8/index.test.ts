9d08d813fb590e1459fd2dd37213cc1da6eaab96
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-10 16:34:18 +0100

    additional tests

--- src/codingMeetup/part8/index.test.ts
+++ src/codingMeetup/part8/index.test.ts
@@ -1,4 +1,5 @@
 import { allContinents } from '.';
+import { fullList } from '../fullList';
 import { TestCaseBoolean } from '../types';
 
 describe(allContinents.name, () => {
@@ -62,8 +63,200 @@ describe(allContinents.name, () => {
         false,
     ];
 
-    const testCases = [testCase1, testCase2];
-    it.each(testCases)(
+    const testCase3: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Mark',
+                lastName: 'G.',
+                country: 'Scotland',
+                continent: 'Europe',
+                age: 22,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Victoria',
+                lastName: 'T.',
+                country: 'Puerto Rico',
+                continent: 'Americas',
+                age: 30,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Emma',
+                lastName: 'B.',
+                country: 'Norway',
+                continent: 'Europe',
+                age: 19,
+                language: 'Clojure',
+            },
+        ],
+        false,
+    ];
+
+    const testCase4: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Europe',
+                age: 29,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Amar',
+                lastName: 'V.',
+                country: 'Bosnia and Herzegovina',
+                continent: 'Europe',
+                age: 32,
+                language: 'Ruby',
+            },
+        ],
+        false,
+    ];
+
+    const testCase5: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Sofia',
+                lastName: 'P.',
+                country: 'Italy',
+                continent: 'Europe',
+                age: 41,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Jayden',
+                lastName: 'P.',
+                country: 'Jamaica',
+                continent: 'Americas',
+                age: 42,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Sou',
+                lastName: 'B.',
+                country: 'Japan',
+                continent: 'Asia',
+                age: 43,
+                language: 'Python',
+            },
+            {
+                firstName: 'Rimas',
+                lastName: 'C.',
+                country: 'Jordan',
+                continent: 'Asia',
+                age: 44,
+                language: 'Java',
+            },
+        ],
+        false,
+    ];
+
+    const testCase6: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Alexander',
+                lastName: 'F.',
+                country: 'Russia',
+                continent: 'Europe',
+                age: 89,
+                language: 'Java',
+            },
+            {
+                firstName: 'Fatima',
+                lastName: 'K.',
+                country: 'Saudi Arabia',
+                continent: 'Asia',
+                age: 21,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Mark',
+                lastName: 'G.',
+                country: 'Scotland',
+                continent: 'Europe',
+                age: 22,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Nikola',
+                lastName: 'H.',
+                country: 'Serbia',
+                continent: 'Europe',
+                age: 29,
+                language: 'Python',
+            },
+            {
+                firstName: 'Jakub',
+                lastName: 'I.',
+                country: 'Slovakia',
+                continent: 'Asia',
+                age: 28,
+                language: 'Java',
+            },
+            {
+                firstName: 'Kseniya',
+                lastName: 'T.',
+                country: 'Belarus',
+                continent: 'Americas',
+                age: 89,
+                language: 'JavaScript',
+            },
+            {
+                firstName: 'Luka',
+                lastName: 'J.',
+                country: 'Slovenia',
+                continent: 'Oceania',
+                age: 29,
+                language: 'Clojure',
+            },
+            {
+                firstName: 'Mariam',
+                lastName: 'B.',
+                country: 'Egypt',
+                continent: 'Africa',
+                age: 89,
+                language: 'Python',
+            },
+        ],
+        true,
+    ];
+
+    const testCase7: TestCaseBoolean = [
+        [
+            {
+                firstName: 'Mehdi',
+                lastName: 'H.',
+                country: 'Tunisia',
+                continent: 'Africa',
+                age: 42,
+                language: 'Python',
+            },
+            {
+                firstName: 'Yusuf',
+                lastName: 'N.',
+                country: 'Turkey',
+                continent: 'Europe',
+                age: 22,
+                language: 'Python',
+            },
+        ],
+        false,
+    ];
+
+    const fullListTestCase: TestCaseBoolean = [fullList, true];
+
+    it.each([
+        testCase1,
+        testCase2,
+        testCase3,
+        testCase4,
+        testCase5,
+        testCase6,
+        testCase7,
+        fullListTestCase,
+    ])(
         'should determine if all continents are represented',
         (developers, allContinentsRepresented) =>
             expect(allContinents(developers)).toEqual(allContinentsRepresented),
