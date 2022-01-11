d2ed6724d092b82f870a2de66ae07c8decb4734f
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-11 21:00:14 +0100

    update types

--- src/codingMeetup/types.ts
+++ src/codingMeetup/types.ts
@@ -1,4 +1,4 @@
-import { CONTINENTS } from './constants';
+import { CONTINENTS, LANGUAGE_LIST } from './constants';
 
 export type Developer = {
     firstName: string;
@@ -6,16 +6,7 @@ export type Developer = {
     country: string;
     continent: typeof CONTINENTS[number];
     age: number;
-    language:
-        | 'Clojure'
-        | 'C'
-        | 'CSS'
-        | 'HTML'
-        | 'Java'
-        | 'JavaScript'
-        | 'PHP'
-        | 'Python'
-        | 'Ruby';
+    language: typeof LANGUAGE_LIST[number];
     githubAdmin?: 'yes' | 'no';
 };
 
@@ -23,12 +14,21 @@ export type LanguageCounts = {
     Clojure?: number;
     C?: number;
     CSS?: number;
+    'C#'?: number;
+    'C++'?: number;
+    CoffeeScript?: number;
+    Crystal?: number;
+    Dart?: number;
+    Elm?: number;
     HTML?: number;
     Java?: number;
     JavaScript?: number;
     PHP?: number;
     Python?: number;
     Ruby?: number;
+    SQL?: number;
+    Swift?: number;
+    TypeScript?: number;
 };
 
 export type TestCaseBoolean = [Developer[], boolean];
