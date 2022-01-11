180dad9ef543e805e9642435528d44517c4cd2c6
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-11 11:51:58 +0100

    Coding Meetup #10 - Higher-Order Functions Series - Create usernames (#27)
    * Coding Meetup #10 - Higher-Order Functions Series - Create usernames init
    
    * tests passing
    
    * mock date to today

---
+++ src/codingMeetup/part10/index.test.ts
@@ -0,0 +1,52 @@
+import { addUsername } from '.';
+import { Developer } from '../types';
+
+type TestCase = [Developer[], ({ username: string } & Developer)[]];
+
+describe(addUsername.name, () => {
+    const testCase1: TestCase = [
+        [
+            {
+                firstName: 'Emily',
+                lastName: 'N.',
+                country: 'Ireland',
+                continent: 'Europe',
+                age: 30,
+                language: 'Ruby',
+            },
+            {
+                firstName: 'Nor',
+                lastName: 'E.',
+                country: 'Malaysia',
+                continent: 'Asia',
+                age: 20,
+                language: 'Clojure',
+            },
+        ],
+        [
+            {
+                firstName: 'Emily',
+                lastName: 'N.',
+                country: 'Ireland',
+                continent: 'Europe',
+                age: 30,
+                language: 'Ruby',
+                username: 'emilyn1992',
+            },
+            {
+                firstName: 'Nor',
+                lastName: 'E.',
+                country: 'Malaysia',
+                continent: 'Asia',
+                age: 20,
+                language: 'Clojure',
+                username: 'nore2002',
+            },
+        ],
+    ];
+    it.each([testCase1])('should add correct username', (input, expected) => {
+        jest.useFakeTimers().setSystemTime(new Date('2022-01-11').getTime());
+
+        expect(addUsername(input)).toEqual(expected);
+    });
+});
