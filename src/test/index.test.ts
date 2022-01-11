1f67ffe0865f52f5f142b34df52dcb3d72dae21d
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-11 13:13:48 +0100

    additional test

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -8,6 +8,12 @@ describe(order.name, () => {
             'Fo1r the2 g3ood 4of th5e pe6ople',
         ],
         ['', ''],
+        [
+            'd4o dru7nken sh2all w5ith s8ailor wha1t 3we a6',
+            'wha1t sh2all 3we d4o w5ith a6 dru7nken s8ailor',
+        ],
+
+        ['3 6 4 2 8 7 5 1 9', '1 2 3 4 5 6 7 8 9'],
     ];
 
     it.each(testCases)('should take "%s" and return "%s"', (input, expected) =>
