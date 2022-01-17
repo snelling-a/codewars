ac3d5bf2bce7cc2b7b048a500be7769e1c1b573e
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-17 22:36:26 +0100

    refactor tests

--- src/learningTypescript/part02_classes_interfaces/part6_getters/index.test.ts
+++ src/learningTypescript/part02_classes_interfaces/part6_getters/index.test.ts
@@ -1,6 +1,6 @@
 import { Cube, Cuboid } from '.';
 
-describe('class Cuboid', () => {
+describe(`class ${Cuboid.name}`, () => {
     it('should initialize properly and have working getters', () => {
         const cuboid = new Cuboid(1, 2, 3);
         expect(cuboid.length).toEqual(1);
@@ -8,22 +8,26 @@ describe('class Cuboid', () => {
         expect(cuboid.height).toEqual(3);
         expect(cuboid.volume).toEqual(6);
         expect(cuboid.surfaceArea).toEqual(22);
+
         cuboid.length = 4;
         expect(cuboid.volume).toEqual(24);
         expect(cuboid.surfaceArea).toEqual(52);
+
         cuboid.width = 5;
         expect(cuboid.volume).toEqual(60);
         expect(cuboid.surfaceArea).toEqual(94);
+
         cuboid.height = 6;
         expect(cuboid.volume).toEqual(120);
         expect(cuboid.surfaceArea).toEqual(148);
+
         [cuboid.length, cuboid.width, cuboid.height] = [7, 8, 9];
         expect(cuboid.volume).toEqual(504);
         expect(cuboid.surfaceArea).toEqual(382);
     });
 });
 
-describe('class Cube extends Cuboid', () => {
+describe(`class ${Cube.name} extends Cuboid`, () => {
     it('should initialize properly and have the same getters as the parent class', () => {
         const cube = new Cube(1);
         expect(cube.length).toEqual(1);
@@ -31,16 +35,20 @@ describe('class Cube extends Cuboid', () => {
         expect(cube.height).toEqual(1);
         expect(cube.volume).toEqual(1);
         expect(cube.surfaceArea).toEqual(6);
-        cube.length = cube.width = cube.height = 2;
+
+        [cube.length, cube.width, cube.height] = [2, 2, 2];
         expect(cube.volume).toEqual(8);
         expect(cube.surfaceArea).toEqual(24);
-        cube.length = cube.width = cube.height = 3;
+
+        [cube.length, cube.width, cube.height] = [3, 3, 3];
         expect(cube.volume).toEqual(27);
         expect(cube.surfaceArea).toEqual(54);
-        cube.length = cube.width = cube.height = 5;
+
+        [cube.length, cube.width, cube.height] = [5, 5, 5];
         expect(cube.volume).toEqual(125);
         expect(cube.surfaceArea).toEqual(150);
-        cube.length = cube.width = cube.height = 10;
+
+        [cube.length, cube.width, cube.height] = [10, 10, 10];
         expect(cube.volume).toEqual(1000);
         expect(cube.surfaceArea).toEqual(600);
     });
