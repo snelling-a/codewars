38d93caf3d01edcc047008af2b77bac1f91cb2da
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-17 22:15:42 +0100

    Learning TypeScript. Classes & Interfaces. Getters init

---
+++ src/learningTypescript/part02_classes_interfaces/part6_getters/index.test.ts
@@ -0,0 +1,47 @@
+import { Cube, Cuboid } from '.';
+
+describe('class Cuboid', () => {
+    it('should initialize properly and have working getters', () => {
+        const cuboid = new Cuboid(1, 2, 3);
+        expect(cuboid.length).toEqual(1);
+        expect(cuboid.width).toEqual(2);
+        expect(cuboid.height).toEqual(3);
+        expect(cuboid.volume).toEqual(6);
+        expect(cuboid.surfaceArea).toEqual(22);
+        cuboid.length = 4;
+        expect(cuboid.volume).toEqual(24);
+        expect(cuboid.surfaceArea).toEqual(52);
+        cuboid.width = 5;
+        expect(cuboid.volume).toEqual(60);
+        expect(cuboid.surfaceArea).toEqual(94);
+        cuboid.height = 6;
+        expect(cuboid.volume).toEqual(120);
+        expect(cuboid.surfaceArea).toEqual(148);
+        [cuboid.length, cuboid.width, cuboid.height] = [7, 8, 9];
+        expect(cuboid.volume).toEqual(504);
+        expect(cuboid.surfaceArea).toEqual(382);
+    });
+});
+
+describe('class Cube extends Cuboid', () => {
+    it('should initialize properly and have the same getters as the parent class', () => {
+        const cube = new Cube(1);
+        expect(cube.length).toEqual(1);
+        expect(cube.width).toEqual(1);
+        expect(cube.height).toEqual(1);
+        expect(cube.volume).toEqual(1);
+        expect(cube.surfaceArea).toEqual(6);
+        cube.length = cube.width = cube.height = 2;
+        expect(cube.volume).toEqual(8);
+        expect(cube.surfaceArea).toEqual(24);
+        cube.length = cube.width = cube.height = 3;
+        expect(cube.volume).toEqual(27);
+        expect(cube.surfaceArea).toEqual(54);
+        cube.length = cube.width = cube.height = 5;
+        expect(cube.volume).toEqual(125);
+        expect(cube.surfaceArea).toEqual(150);
+        cube.length = cube.width = cube.height = 10;
+        expect(cube.volume).toEqual(1000);
+        expect(cube.surfaceArea).toEqual(600);
+    });
+});
---
+++ src/learningTypescript/part02_classes_interfaces/part6_getters/index.ts
@@ -0,0 +1,33 @@
+/* eslint-disable max-classes-per-file */
+/*
+https://www.codewars.com/kata/599d636e607719edf600010a
+
+Define the following classes:
+
+I. Cuboid
+The object constructor for the class Cuboid should receive exactly three arguments in the following order:
+length, width, height and store these three values in this.length, this.width and this.height respectively.
+
+The class Cuboid should then have a getter surfaceArea which returns the surface area of the cuboid and a
+getter volume which returns the volume of the cuboid.
+
+II. Cube
+Class Cube is a subclass of class Cuboid. The constructor function of Cube should receive one argument only,
+its length, and use that value passed in to set this.length, this.width and this.height.
+
+Hint: Make a call to super, passing in the correct arguments, to make life easier ;)
+
+Articles of Interest
+Below are some articles of interest that may help you complete this Kata:
+
+Stack Overflow - What are getters and setters in ES6? (http://stackoverflow.com/questions/28222276/what-are-getters-and-setters-for-in-ecmascript-6-classes)
+getter - Javascript | MDN (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get)
+*/
+
+export class Cuboid {
+    // TODO:
+}
+
+export class Cube extends Cuboid {
+    // TODO:
+}
