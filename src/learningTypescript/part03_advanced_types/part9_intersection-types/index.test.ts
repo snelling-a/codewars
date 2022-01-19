ba5b7582bed61da0428ed17064044aa067078f98
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-19 10:39:13 +0100

    additional tests

--- src/learningTypescript/part03_advanced_types/part9_intersection-types/index.test.ts
+++ src/learningTypescript/part03_advanced_types/part9_intersection-types/index.test.ts
@@ -11,4 +11,35 @@ describe(intersect.name, () => {
         expect(intersection2).toEqual({});
     });
 
+    it('should work correctly with empty objects', () => {
+        const obj1 = {};
+        const obj2 = { a: Math.random(), b: Math.random() };
+        const intersection3 = intersect(obj1, obj2);
+        expect(intersection3).toEqual({});
+        const intersection4 = intersect(obj2, obj1);
+        expect(intersection4).toEqual({});
+        const intersection5 = intersect(obj1, obj1);
+        expect(intersection5).toEqual({});
+    });
+});
+
+it.each([
+    [{ a: 1 }, { a: 2 }, { a: 1 }],
+    [
+        { a: 1, b: 2 },
+        { a: 2, b: 3 },
+        { a: 1, b: 2 },
+    ],
+    [
+        { a: 1, b: 2 },
+        { a: 2, b: 3, c: 4 },
+        { a: 1, b: 2 },
+    ],
+    [
+        { a: 1, b: 2 },
+        { a: 2, b: 3, c: 4, d: 5 },
+        { a: 1, b: 2 },
+    ],
+])('should return intersection of %p and %p as %p', (obj1, obj2, expected) => {
+    expect(intersect(obj1, obj2)).toEqual(expected);
 });
