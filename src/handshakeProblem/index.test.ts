530364e402f9d7e84a000f90459f17e538072c0a
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-23 21:32:08 +0100

    additional tests

--- src/handshakeProblem/index.test.ts
+++ src/handshakeProblem/index.test.ts
@@ -7,7 +7,18 @@ describe('Handshake problem', () => {
         [3, 3],
         [6, 4],
         [7, 5],
-    ])('should test for fixed cases', (input, expected) =>
+    ])('should take %d handshakes and return %d people', (input, expected) =>
         expect(getParticipants(input)).toBe(expected),
+    );
+
+    it.each([
+        ...Array.from({ length: 100 }, () => {
+            const randomNumber = Math.round(Math.random() * 999 + 5);
+
+            return [(randomNumber * (randomNumber - 1)) / 2, randomNumber];
+        }),
+    ])(
+        'should take a random number of people %d and return %d handshakes',
+        (input, expected) => expect(getParticipants(input)).toBe(expected),
     );
 });
