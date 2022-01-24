06c0eb7fe7b4ebefc080adc4fbd7b1486ec22b3a
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-24 13:05:37 +0100

    refactor test for clarity

--- src/handshakeProblem/index.test.ts
+++ src/handshakeProblem/index.test.ts
@@ -1,24 +1,30 @@
 import { getParticipants } from '.';
 
-describe('Handshake problem', () => {
+describe(getParticipants.name, () => {
     it.each([
         [0, 1],
         [1, 2],
         [3, 3],
         [6, 4],
         [7, 5],
-    ])('should take %d handshakes and return %d people', (input, expected) =>
-        expect(getParticipants(input)).toBe(expected),
+    ])(
+        'should take %d handshakes and return %d participants',
+        (handshakes, participants) =>
+            expect(getParticipants(handshakes)).toBe(participants),
     );
+
+    const getHandshakes = (participants: number) =>
+        (participants * (participants - 1)) / 2;
 
     it.each([
         ...Array.from({ length: 100 }, () => {
-            const randomNumber = Math.round(Math.random() * 999 + 5);
+            const randomNumberOfPeople = Math.round(Math.random() * 999 + 5);
 
-            return [(randomNumber * (randomNumber - 1)) / 2, randomNumber];
+            return [getHandshakes(randomNumberOfPeople), randomNumberOfPeople];
         }),
     ])(
-        'should take a random number of people %d and return %d handshakes',
-        (input, expected) => expect(getParticipants(input)).toBe(expected),
+        'should take a random number of handshakes, %d, and return %d participants',
+        (handshakes, participants) =>
+            expect(getParticipants(handshakes)).toBe(participants),
     );
 });
