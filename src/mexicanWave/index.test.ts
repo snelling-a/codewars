dc32662dd04ed62661550ee6f695fc5e54fdbf70
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-30 20:54:16 +0100

    Mexican Wave (#54)
    * Mexican Wave init
    
    * tests passing
    
    * refactor for clarity

---
+++ src/mexicanWave/index.test.ts
@@ -0,0 +1,40 @@
+import { wave } from '.';
+
+describe(wave.name, () => {
+    describe('Sample Tests', () => {
+        it.each([
+            ['hello', ['Hello', 'hEllo', 'heLlo', 'helLo', 'hellO']],
+            [
+                'codewars',
+                [
+                    'Codewars',
+                    'cOdewars',
+                    'coDewars',
+                    'codEwars',
+                    'codeWars',
+                    'codewArs',
+                    'codewaRs',
+                    'codewarS',
+                ],
+            ],
+            [
+                'two words',
+                [
+                    'Two words',
+                    'tWo words',
+                    'twO words',
+                    'two Words',
+                    'two wOrds',
+                    'two woRds',
+                    'two worDs',
+                    'two wordS',
+                ],
+            ],
+            ['', []],
+            [' gap ', [' Gap ', ' gAp ', ' gaP ']],
+        ])(
+            'should take the string "%s" and return a wave: %s',
+            (input, output) => expect(wave(input)).toStrictEqual(output),
+        );
+    });
+});
