ec88cab948b293b1a388ab7e9de874f74d36bb9b
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-01-31 23:38:45 +0100

    tests passing

--- src/sumOfIntervals/index.ts
+++ src/sumOfIntervals/index.ts
@@ -1,10 +1,12 @@
 /*
 https://www.codewars.com/kata/52b7ed099cdc285c300001cd
 
-Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of
+all the interval lengths. Overlapping intervals should only be counted once.
 
 Intervals
-Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be
+less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
 
 Overlapping Intervals
 List containing overlapping intervals:
@@ -23,5 +25,11 @@ sumOfIntervals([[1, 5]]) // => 4
 */
 
 export function sumOfIntervals(intervals: [number, number][]) {
-    // your code here
+    return intervals.reduce<Map<string, number>>((acc, [a, b]) => {
+        for (let i = a; i < b; i++) {
+            acc.set(`${i}`, 1);
+        }
+
+        return acc;
+    }, new Map()).size;
 }
