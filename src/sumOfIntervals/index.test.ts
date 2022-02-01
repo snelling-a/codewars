55ada581f1cea53487c1d83c51075f1418726399
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-02-01 21:14:49 +0100

    Sum of Intervals (#58)
    * Sum of Intervals init
    
    * add example test with negative number
    
    * tests passing
    
    * change map to set to optimise

---
+++ src/sumOfIntervals/index.test.ts
@@ -0,0 +1,41 @@
+import { sumOfIntervals } from '.';
+
+describe(sumOfIntervals.name, () => {
+    describe('basic tests', () => {
+        it.each<[[number, number][], number]>([
+            [[[1, 5]], 4],
+            [
+                [
+                    [1, 5],
+                    [6, 10],
+                ],
+                8,
+            ],
+            [
+                [
+                    [1, 5],
+                    [1, 5],
+                ],
+                4,
+            ],
+            [
+                [
+                    [1, 4],
+                    [7, 10],
+                    [3, 5],
+                ],
+                7,
+            ],
+            [
+                [
+                    [1, 5],
+                    [10, 15],
+                    [-1, 3],
+                ],
+                11,
+            ],
+        ])('should take intervals %j and return %d', (intervals, expected) =>
+            expect(sumOfIntervals(intervals)).toEqual(expected),
+        );
+    });
+});
