3e4b9c4e2fcdcb7c99ae36c20817fef390f8bd3e
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-02-05 06:17:03 +0100

    tests passing

--- src/reversedSequence/index.ts
+++ src/reversedSequence/index.ts
@@ -6,6 +6,5 @@ Build a function that returns an array of integers from n to 1 where n>0.
 Example : n=5 --> [5,4,3,2,1]
 */
 
-export const reverseSeq = (n: number): number[] => {
-    return [];
-};
+export const reverseSeq = (n: number): number[] =>
+    Array.from({ length: n }, (_, i) => n - i);
