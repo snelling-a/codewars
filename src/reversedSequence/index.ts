933dcb54aacae0610556dfb44d2cd471d4d079d3
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-02-05 06:27:59 +0100

    Reversed sequence (#59)
    * Reversed sequence init
    
    * tests passing
    
    * additional tests

---
+++ src/reversedSequence/index.ts
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+export const reverseSeq = (n: number): number[] =>
+    Array.from({ length: n }, (_, i) => n - i);
