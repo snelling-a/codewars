bdad7c77d2bc56c8f73b00191db72df96a816d43
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-02-05 06:24:06 +0100

    additional tests

--- src/reversedSequence/index.test.ts
+++ src/reversedSequence/index.test.ts
@@ -1,6 +1,16 @@
 import { reverseSeq } from '.';
 
 describe(reverseSeq.name, () => {
-    it('should pass a simple test', () =>
-        expect(reverseSeq(5)).toEqual([5, 4, 3, 2, 1]));
+    describe('basic tests', () => {
+        it.each([
+            [5, [5, 4, 3, 2, 1]],
+            [6, [6, 5, 4, 3, 2, 1]],
+            [100, new Array(100).fill(0).map((_, i) => 100 - i)],
+            [10_000, new Array(10000).fill(0).map((_, i) => 10000 - i)],
+            [100_000, new Array(100000).fill(0).map((_, i) => 100000 - i)],
+            [1_000_000, new Array(1000000).fill(0).map((_, i) => 1000000 - i)],
+        ])('should accept %d and return %j', (n, expected) =>
+            expect(reverseSeq(n)).toEqual(expected),
+        );
+    });
 });
