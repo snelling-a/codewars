3561b947b0e00914ab0329c85f979709f571dccf
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-04-09 22:17:20 +0200

    refactor test

--- src/alphabetSymmetry/index.test.ts
+++ src/alphabetSymmetry/index.test.ts
@@ -1,17 +1,26 @@
 import { solve } from '.';
 
-describe('Fixed tests', () => {
-    it('Testing for ["abode", "ABc", "xyzD"]', () =>
-        expect(solve(['abode', 'ABc', 'xyzD'])).toEqual([4, 3, 1]));
-
-    it('Testing for ["abide", "ABc", "xyz"]', () =>
-        expect(solve(['abide', 'ABc', 'xyz'])).toEqual([4, 3, 0]));
-
-    it('Testing for ["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"]', () =>
-        expect(solve(['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc'])).toEqual([
-            6, 5, 7,
-        ]));
-
-    it('Testing for ["encode", "abc", "xyzD", "ABmD"]', () =>
-        expect(solve(['encode', 'abc', 'xyzD', 'ABmD'])).toEqual([1, 3, 1, 3]));
+describe('Alphabet Symmetry', () => {
+    describe('Fixed tests', () => {
+        it.each([
+            [
+                ['abode', 'ABc', 'xyzD'],
+                [4, 3, 1],
+            ],
+            [
+                ['abide', 'ABc', 'xyz'],
+                [4, 3, 0],
+            ],
+            [
+                ['IAMDEFANDJKL', 'thedefgh', 'xyzDEFghijabc'],
+                [6, 5, 7],
+            ],
+            [
+                ['encode', 'abc', 'xyzD', 'ABmD'],
+                [1, 3, 1, 3],
+            ],
+        ])('Testing for %j should result in %j', (arr, expected) =>
+            expect(solve(arr)).toEqual(expected),
+        );
+    });
 });
