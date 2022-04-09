d552720cfa06222851e60cdda823349966d127d3
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2022-04-09 17:00:02 +0200

    add url

--- src/filterOutTheGeese/index.ts
+++ src/filterOutTheGeese/index.ts
@@ -1,4 +1,6 @@
 /*
+https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7/
+
 Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements
 but with the 'geese' removed.
 
